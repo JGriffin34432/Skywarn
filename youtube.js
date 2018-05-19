4cf4d75235e05daf592f558f8d0458d5b9072beb
Joshua Griffin <JoshTheGamer632@outlook.com>
2018-05-19 11:18:54 -0400

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,6 +1,6 @@
 var request = require('superagent');
 
-const API_KEY = "GOOGLE API KEY GOES HERE";
+const API_KEY = (process.env.API_KEY);
 const WATCH_VIDEO_URL = "https://www.youtube.com/watch?v=";
 
 exports.watchVideoUrl = WATCH_VIDEO_URL;
